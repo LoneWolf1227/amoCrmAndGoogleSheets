8246606febbaa025320dee8018b08c8b353b95eb
LoneWolf1227 <48411716+LoneWolf1227@users.noreply.github.com>
2020-06-12 09:25:49 -0400

    Update config.php

--- config.php
+++ config.php
@@ -1,12 +1,6 @@
 <?php
 
 //Доступ к amoCRM
-define('AMO_DOMAIN', 'manalonewolf');
-define('AMO_API', '7adc591a56a7747af9a257f15be0d32404279948');
-define('AMO_LOGIN', 'manalonewolf@gmail.com');
-
-//Настройки БД
-define('DB_HOST', 'localhost');
-define('DB_LOGIN', 'id13554751_wolfalone');
-define('DB_PASS', 'O-6fBEe+%p*GP#+j');
-define('DB_DATABASE', 'id13554751_project_test');
+define('AMO_DOMAIN', 'yourSubDomain');
+define('AMO_API', 'apiHash');
+define('AMO_LOGIN', 'yourLogin');
